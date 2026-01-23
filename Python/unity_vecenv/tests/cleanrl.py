862bb2e48840356fce9ed94c2517c7a2b1aab332
Mart Kartasev <mart.kartasev@gmail.com>
2026-01-23 13:38:30 +0100

    CleanRL params

--- Python/unity_vecenv/tests/cleanrl.py
+++ Python/unity_vecenv/tests/cleanrl.py
@@ -46,7 +46,7 @@ class Args:
     # Algorithm specific arguments
     env_id: str = "Watercraft-v1"
     """the id of the environment"""
-    total_timesteps: int = 30000000
+    total_timesteps: int = 50000000
     """total timesteps of the experiments"""
     learning_rate: float = 3e-4
     """the learning rate of the optimizer"""
